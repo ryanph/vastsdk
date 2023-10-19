cc079db254a4cf81e4ef07a518d0d9b9c3851471
Ryan Parker-Hill <ryanph@aspersion.net>
2023-10-19 16:43:05 +1100

    Added Quotas

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,1 @@
-Numerous additions and changes to correct errors in the spec
-Added User and Group query to easily lookup SIDs, Missing query parameters for Views and View Policies
+Added Quotas
---
+++ examples/python/vast_quotas.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import vast_token
+import logging
+from vastsdk.api import QuotasApi
+
+log = logging.getLogger("vast_quotas")
+logging.basicConfig(level=logging.DEBUG)
+
+settings = vast_token.read_settings()
+vastApiClient = vast_token.create_client()
+vastQuotasApi = QuotasApi(vastApiClient)
+
+logging.info("Fetching all quotas")
+quotas = vastQuotasApi.get_quotas()
+logging.info(quotas)
+
+logging.info("Creating a new quota")
+new_quota = vastQuotasApi.create_quota(settings['test_quota_data'])
+logging.info(new_quota)
+
+logging.info("Fetching a specific quota")
+specific_quota = vastQuotasApi.get_quota_by_id(id=new_quota.id)
+logging.info(specific_quota)
+
+logging.info("Updating Quota")
+updated_quota = vastQuotasApi.update_quota_by_id(id=new_quota.id, quota={"hard_limit": settings['test_quota_data']['hard_limit'] * 2})
+logging.info(updated_quota)
+
+logging.info("Deleting Quota")
+vastQuotasApi.delete_quota_by_id(id=new_quota.id)
