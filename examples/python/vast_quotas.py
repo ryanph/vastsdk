0636a3973112e16510fdb442a9da26e472e66b9f
Ryan Parker-Hill <ryanph@aspersion.net>
2023-10-19 20:30:44 +1100

    Added path search for quotas

--- examples/python/vast_quotas.py
+++ examples/python/vast_quotas.py
@@ -12,7 +12,7 @@ vastApiClient = vast_token.create_client()
 vastQuotasApi = QuotasApi(vastApiClient)
 
 logging.info("Fetching all quotas")
-quotas = vastQuotasApi.get_quotas()
+quotas = vastQuotasApi.list_quotas()
 logging.info(quotas)
 
 logging.info("Creating a new quota")
@@ -21,6 +21,14 @@ logging.info(new_quota)
 
 logging.info("Fetching a specific quota")
 specific_quota = vastQuotasApi.get_quota_by_id(id=new_quota.id)
+logging.info(specific_quota)
+
+logging.info("Search for a specific quota by name")
+specific_quota = vastQuotasApi.list_quotas(name=settings['test_quota_data']['name'])
+logging.info(specific_quota)
+
+logging.info("Search for a specific quota by path")
+specific_quota = vastQuotasApi.list_quotas(path=settings['test_quota_data']['path'])
 logging.info(specific_quota)
 
 logging.info("Updating Quota")
